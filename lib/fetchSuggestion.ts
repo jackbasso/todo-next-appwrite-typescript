cd1218dc85099af497c42c4cbcdb89eaa4b92e31
jackbasso <jackbasso.r@gmail.com>
2023-07-13 19:25:13 -0600

    integration with openai

---
+++ lib/fetchSuggestion.ts
@@ -0,0 +1,22 @@
+
+import formatTodosForAI from "./formatTodosForAI";
+
+const fetchSuggestion = async (board: Board) => {
+  const todos = formatTodosForAI(board);
+  console.log("FORMATTED TODOS to send >>", todos);
+
+  const res = await fetch("/api/generateSummary", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ todos }),
+  });
+
+  const GPTdata = await res.json();
+  const { content } = GPTdata;
+
+  return content;
+}
+
+export default fetchSuggestion;
